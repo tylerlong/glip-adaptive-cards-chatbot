716a3dfc5064bdf1992ffdbd5df86750678ebf33
Tyler Long <tyler4long@gmail.com>
2021-07-29 12:06:03 -0700

    Try to render adapative cards

--- src/lambda.ts
+++ src/lambda.ts
@@ -1,3 +1,4 @@
+import RestException from '@rc-ex/core/lib/RestException';
 const createApp = require('ringcentral-chatbot/dist/apps').default;
 const {createAsyncProxy} = require('ringcentral-chatbot/dist/lambda');
 const serverlessHTTP = require('serverless-http');
@@ -5,9 +6,45 @@ const axios = require('axios');
 
 const handle = async (event: any) => {
   const {type, text, group, bot} = event;
-  if (type === 'Message4Bot' && text === 'ping') {
-    console.log(text);
-    await bot.sendMessage(group.id, {text: 'pong'});
+  if (type === 'Message4Bot') {
+    if (text === 'ping') {
+      await bot.sendMessage(group.id, {text: 'pong'});
+    } else if (text.startsWith('[code]') && text.endsWith('[/code]')) {
+      let code = text.trim().substring(6).trim();
+      code = code.substring(0, code.length - 7).trim();
+      let obj: any;
+      try {
+        obj = JSON.parse(code);
+      } catch (e) {
+        await bot.sendMessage(group.id, {
+          text: 'The code your shared is not a valid JSON object',
+        });
+        return;
+      }
+      if (obj.type !== 'AdaptiveCard') {
+        await bot.sendMessage(group.id, {
+          text: 'The JSON your shared is not for Adaptive Cards',
+        });
+        return;
+      }
+      try {
+        await bot.rc.post(
+          `/restapi/v1.0/glip/chats/${group.id}/adaptive-cards`,
+          obj
+        );
+      } catch (e) {
+        await bot.sendMessage(group.id, {
+          text: `We got an exception when trying to render the Adaptive Card: [code]${
+            (e as RestException).response.data
+          }[/code]`,
+        });
+        (e as RestException).message;
+      }
+    } else {
+      await bot.sendMessage(group.id, {
+        text: 'Please share me a code snippet of an Adaptive Card and I will render it for you',
+      });
+    }
   }
 };
 const app = createApp(handle);
