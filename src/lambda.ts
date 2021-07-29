58c0a4c65266a30a5a6f8ab5bcba8f021ceedcb2
Tyler Long <tyler4long@gmail.com>
2021-07-29 08:00:31 -0700

    First complete version

---
+++ src/lambda.ts
@@ -0,0 +1,26 @@
+const createApp = require('ringcentral-chatbot/dist/apps').default;
+const {createAsyncProxy} = require('ringcentral-chatbot/dist/lambda');
+const serverlessHTTP = require('serverless-http');
+const axios = require('axios');
+
+const handle = async (event: any) => {
+  const {type, text, group, bot} = event;
+  if (type === 'Message4Bot' && text === 'ping') {
+    console.log(text);
+    await bot.sendMessage(group.id, {text: 'pong'});
+  }
+};
+const app = createApp(handle);
+module.exports.app = serverlessHTTP(app);
+module.exports.proxy = createAsyncProxy('app');
+module.exports.maintain = async () =>
+  axios.put(
+    `${process.env.RINGCENTRAL_CHATBOT_SERVER}/admin/maintain`,
+    undefined,
+    {
+      auth: {
+        username: process.env.RINGCENTRAL_CHATBOT_ADMIN_USERNAME,
+        password: process.env.RINGCENTRAL_CHATBOT_ADMIN_PASSWORD,
+      },
+    }
+  );
